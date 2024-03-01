5f7f5862a9e7d8b87f13ec2261484e0a3eb0a502
Frosty <frosticles922@gmail.com>
2024-03-02 07:33:21 +0800

    added error handling

--- main.py
+++ main.py
@@ -19,6 +19,14 @@ def create_app():
     ma.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+    
+    @app.errorhandler(400)
+    def bad_request(err):
+        return {"error": str(err)}, 400
+    
+    @app.errorhandler(404)
+    def not_found(err):
+        return {"error": str(err)}, 404
         
     @app.errorhandler(ValidationError)
     def validation_error(error):
